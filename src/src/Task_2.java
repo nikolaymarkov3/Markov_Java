e7f83b101c13781ab1c5bffc94e5c5aa6b3f8729
Nikolai Markow <i@nikomar.ru>
2023-11-13 18:14:23 +0700

    add тз Марков Java

--- src/src/Task_2.java
+++ src/src/Task_2.java
@@ -3,17 +3,16 @@ package src;
 import java.util.Scanner;
 
 public class Task_2 {
-
     public static void main(String[] args) {
         /*
         Составить алгоритм: если введенное имя совпадает с Вячеслав, то вывести “Привет, Вячеслав”,
-         если нет, то вывести "Нет такого имени"
+        если нет, то вывести "Нет такого имени"
          */
 
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
 
-        if (name.equals("Вячеслав")){
+        if (name.equals("Вячеслав")) {
             System.out.println("Привет ".concat(name));
         }else {
             System.out.println("Нет такого имени");
